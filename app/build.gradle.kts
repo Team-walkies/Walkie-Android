4d987e1c9da223cb7c9ad7d8c319f1c4de213a6b
IslandOfDream <rhffldkt0517@naver.com>
2025-05-10 16:24:20 +0900

    build walkie 1.0.0(3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     namespace = "com.startup.walkie"
     defaultConfig {
         applicationId = "com.startup.walkie"
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
         targetSdk = Const.TARGET_SDK
         val nativeAppKey = properties.getProperty("NATIVE_APP_KEY")
