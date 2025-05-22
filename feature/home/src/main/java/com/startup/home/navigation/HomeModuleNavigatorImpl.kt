ccaff75b50a7c761e2b4a523937133d08abb7355
minho <kr1227kr@flow.team>
2025-05-22 23:33:59 +0900

    #199 알림으로 앱 접근 시 홈 화면 2개 있음

--- feature/home/src/main/java/com/startup/home/navigation/HomeModuleNavigatorImpl.kt
+++ feature/home/src/main/java/com/startup/home/navigation/HomeModuleNavigatorImpl.kt
@@ -6,8 +6,13 @@ import com.startup.home.HomeActivity
 import com.startup.navigation.HomeModuleNavigator
 import javax.inject.Inject
 
-class HomeModuleNavigatorImpl @Inject constructor(): HomeModuleNavigator{
+class HomeModuleNavigatorImpl @Inject constructor() : HomeModuleNavigator {
     override fun moveToHomeActivity(context: Context) {
-        context.startActivity(Intent(context, HomeActivity::class.java))
+        context.startActivity(
+            Intent(context, HomeActivity::class.java)
+                .apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+        )
     }
 }
