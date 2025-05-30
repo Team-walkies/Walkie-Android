f536486dd7914d46c03d7ef1dbe674558e48fe0d
IslandOfDream <rhffldkt0517@naver.com>
2025-05-30 15:38:32 +0900

    #205 알 부화처리시 최신 알 정보를 가져오도록 수정 / 알 바꿀때 현재 걸음수를 서버에 업로드 하고 바꾸도록 수정

--- feature/home/src/main/java/com/startup/home/egg/GainEggViewModel.kt
+++ feature/home/src/main/java/com/startup/home/egg/GainEggViewModel.kt
@@ -1,13 +1,15 @@
 package com.startup.home.egg
 
 import androidx.lifecycle.viewModelScope
+import com.startup.common.base.BaseUiState
 import com.startup.common.base.BaseViewModel
 import com.startup.common.event.EventContainer
-import com.startup.common.util.Printer
-import com.startup.domain.provider.StepDataStore
-import com.startup.common.base.BaseUiState
 import com.startup.common.event.EventContainer.triggerNotificationUpdate
+import com.startup.common.util.Printer
+import com.startup.domain.model.egg.UpdateStepData
+import com.startup.domain.provider.StepDataStore
 import com.startup.domain.usecase.egg.GetGainEggList
+import com.startup.domain.usecase.egg.UpdateEggOfStepCount
 import com.startup.domain.usecase.walk.UpdateWalkingEgg
 import com.startup.model.egg.MyEggModel.Companion.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,13 +21,16 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GainEggViewModel @Inject constructor(
     getGainEggList: GetGainEggList,
     private val updateWalkingEgg: UpdateWalkingEgg,
+    private val updateEggOfStepCount: UpdateEggOfStepCount,
     private val dataStore: StepDataStore
 ) : BaseViewModel() {
 
@@ -44,13 +49,38 @@ class GainEggViewModel @Inject constructor(
     override val state: GainEggViewState get() = _state
 
     fun updateEgg(eggId: Long, needStep: Int, nowStep: Int) {
+        viewModelScope.launch {
+            val currentWalkEggId = dataStore.getCurrentWalkEggId()
+            if (currentWalkEggId != 0L && currentWalkEggId != eggId) {
+                val currentSteps = dataStore.getEggCurrentSteps()
+
+                updateEggOfStepCount.invoke(
+                    UpdateStepData(
+                        eggId = currentWalkEggId,
+                        nowStep = currentSteps
+                    )
+                ).onCompletion {
+                    // 저장 완료 후 새로운 알로 변경
+                    updateNewEgg(eggId, needStep, nowStep)
+                }.catch {
+                    Printer.e("GainEggViewModel", "Failed to save previous egg steps")
+                    updateNewEgg(eggId, needStep, nowStep)
+                }.launchIn(viewModelScope)
+            } else {
+                // 이전 알이 없으면 바로 새로운 알로 변경
+                updateNewEgg(eggId, needStep, nowStep)
+            }
+        }
+    }
+
+    private fun updateNewEgg(eggId: Long, needStep: Int, nowStep: Int) {
         updateWalkingEgg.invoke(eggId)
             .onEach {
                 notifyViewModelEvent(GainEggViewModelEvent.FetchEggList)
                 EventContainer.onRefreshEvent()
                 if (needStep > 0) {
                     dataStore.setHatchingTargetStep(target = needStep)
-                    dataStore.saveEggCurrentSteps(steps = nowStep) // 알이 변경되었으므로 걸음수를 eggModel의 nowStep으로 초기화
+                    dataStore.saveEggCurrentSteps(steps = nowStep)
                     dataStore.setCurrentWalkEggId(eggId = eggId)
                     triggerNotificationUpdate(targetStep = needStep)
                     Printer.d(
