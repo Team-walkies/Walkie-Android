8663418cf69bab9158f8e9e5b1e706a78daf5ce1
minho <my_alsgh@naver.com>
2025-11-17 09:57:58 +0900

    build walkie 1.0.8(18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     namespace = "com.startup.walkie"
     defaultConfig {
         applicationId = "com.startup.walkie"
-        versionCode = 17
+        versionCode = 18
         versionName = "1.0.8"
         targetSdk = Const.TARGET_SDK
         val nativeAppKey = properties.getProperty("NATIVE_APP_KEY")
